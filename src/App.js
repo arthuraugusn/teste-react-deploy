c2091776c35dcc6097cb7e116a5563e3fc207567
Arthur Augusto <98403987+arthuraugusn@users.noreply.github.com>
2023-03-24 10:35:36 -0300

    commit1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import Sobre from "./Sobre";
@@ -6,13 +6,11 @@ import Usuario from "./Usuario";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route Component={Home} path="/" />
-        <Route Component={Sobre} path="/sobre" />
-        <Route Component={Usuario} path="/usuario" />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route Component={Home} path="/" />
+      <Route Component={Sobre} path="/sobre" />
+      <Route Component={Usuario} path="/usuario" />
+    </Routes>
   );
 }
 
